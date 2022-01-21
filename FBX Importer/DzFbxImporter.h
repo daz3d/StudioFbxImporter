44bfa5548e7df6378a1bc71bf22120100824d52b
Vish Atlu <vatlu@daz3d.com>
2022-01-20 16:55:34 -0800

    Updated copyright text in the source files.

--- FBX Importer/DzFbxImporter.h
+++ FBX Importer/DzFbxImporter.h
@@ -1,11 +1,19 @@
 /**********************************************************************
-	Copyright (C) 2012-2021 Daz 3D, Inc. All Rights Reserved.
+	Copyright (C) 2012-2022 DAZ 3D, Inc. All Rights Reserved.
 
-	This is UNPUBLISHED PROPRIETARY SOURCE CODE of Daz 3D, Inc;
-	the contents of this file may not be disclosed to third parties,
-	copied or duplicated in any form, in whole or in part, without the
-	prior written permission of Daz 3D, Inc.
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
 **********************************************************************/
+
 #pragma once
 
 /****************************
--- cmake/DzMocFiles.cmake
+++ cmake/DzMocFiles.cmake
@@ -1,10 +1,17 @@
 #######################################################################
-#	Copyright (C) 2016 DAZ 3D, Inc. All Rights Reserved.
+#	Copyright (C) 2016-2022 DAZ 3D, Inc. All Rights Reserved.
 #
-#	This is UNPUBLISHED PROPRIETARY SOURCE CODE of DAZ 3D, Inc.
-#	the contents of this file may not be disclosed to third parties,
-#	copied or duplicated in any form, in whole or in part, without the
-#	prior written permission of DAZ 3D, Inc.
+#	Licensed under the Apache License, Version 2.0 (the "License");
+#	you may not use this file except in compliance with the License.
+#	You may obtain a copy of the License at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+#
+#	Unless required by applicable law or agreed to in writing, software
+#	distributed under the License is distributed on an "AS IS" BASIS,
+#	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#	See the License for the specific language governing permissions and
+#	limitations under the License.
 #######################################################################
 
 macro( dz_moc_headers is_qt5_moc addtional_options outputList )
